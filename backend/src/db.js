bbdc578c24314ec29b1fbf1f83eab0a028a9e6be
divv919 <divyanshsoni919@gmail.com>
2025-06-24 15:59:32 +0530

    fix

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,16 +1,17 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
-const sequelize = new Sequelize(
-  process.env.DB_DATABASE,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-  }
-);
+
+const sequelize = new Sequelize(process.env.POSTGRES_URL_NON_POOLING, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 const connectAndSyncDB = async () => {
   try {
     await sequelize.authenticate();
